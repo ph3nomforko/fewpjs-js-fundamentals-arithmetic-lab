9df9d7395fc2025caeefd6a6fc38dbabbc13c98a
Codi Kraus <codi.kraus@gmail.com>
2021-09-21 12:00:50 -0500

    commit tree c376fdaeb81fceb7ebec6a71e625102594764fe1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 // Write your code here
+
+const random = Math.floor(Math.random() > 0);
+
+let num3 = 10;
+let num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(19, 20);
